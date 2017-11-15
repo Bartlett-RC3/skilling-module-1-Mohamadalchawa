ba5c2d592eee6f645715cd5271edc3ca44aebea6
Mohamadalchawa <ucqbma9@ucl.ac.uk>
2017-11-15 18:24:58 +0000

    Session 2 homework
    homework 2 missing class declaration

---
+++ Assets/Scripts/Homework/Session2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session2 : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        condition();
+		
+	}
+
+    void condition()
+    {
+        int value = 50;
+        if (value == 50)
+        {
+            print("value is 50");
+        }
+        else
+        {
+            print("value is not 50");
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            print("loop Value is " + i);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
