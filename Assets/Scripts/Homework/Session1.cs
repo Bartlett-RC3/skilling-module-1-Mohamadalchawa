d44c7d27697c9c0229a9fa177f3e0a8db54ee528
Mohamadalchawa <ucqbma9@ucl.ac.uk>
2017-11-15 17:04:48 +0000

    Completed homework 1
    completed homework one , added the dictionary and fixed the list by making it public as it wasnt showing .

--- Assets/Scripts/Homework/Session1.cs
+++ Assets/Scripts/Homework/Session1.cs
@@ -14,7 +14,9 @@ public class Session1 : MonoBehaviour {
     private float firstFloat = 1.5f;
     private string firstString = "These are the declared variables : ";
     public int[] firstArray;
-    private List<float> firstList;
+    public List<float> firstList;
+    Dictionary<string,string> rc3Membeers = new Dictionary<string, string>();
+
 
     // Use this for initialization
     void Start ()
@@ -30,6 +32,12 @@ public class Session1 : MonoBehaviour {
         firstList = new List<float>();
         firstList.Add(4);
         firstList.Add(3);
+
+        rc3Membeers.Add("slot1","jelena");
+        rc3Membeers.Add("slot2","noura");
+
+        print("The number of RC3 members added in this dictionary is "+ rc3Membeers.Count);
+        print("They are " + rc3Membeers["slot1"] + " and " + rc3Membeers["slot2"]);
 
     }
 
